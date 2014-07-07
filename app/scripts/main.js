b40daf27c3efbc0b50625b1194d2ab672dc6ad53
Zach Wentz <zkwentz@gmail.com>
2014-07-06 22:24:21 -0700

    decoupling sudoku from presentation code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,34 +13,7 @@ requirejs.config({
 });
 
 require(['jquery','underscore','sudoku','templates'],function($,_,sudoku,templates){
-  $(document).on('input','.sudoku-square',function(e){
-    var $this = $(this);
-    var $enteredValue = $this.html();
-    if ($enteredValue.length > 1)
-      {
-        //shake box
-      }
-    $this.html($enteredValue.slice(0,1));
-  })
-  .on('keydown','.sudoku-square',function(e){
-    // Allow: backspace, delete, tab, escape, enter and .
-    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-         // Allow: Ctrl+A
-        (e.keyCode == 65 && e.ctrlKey === true) ||
-         // Allow: home, end, left, right
-        (e.keyCode >= 35 && e.keyCode <= 39)) {
-             // let it happen, don't do anything
-
-             if (e.keyCode === 8)
-               $(this).html("");
-             return;
-    }
-    // Ensure that it is a number and stop the keypress (1-9)
-    if ((e.shiftKey || (e.keyCode < 49 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-        e.preventDefault();
-    }
-  })
-  .on('click touch','#pauseGame:not(.is-paused)',function(e){
+  $(document).on('click touch','#pauseGame:not(.is-paused)',function(e){
     $('.flippable').removeClass('is-flipped');
   })
   .on('click touch','.difficulty-option',function(e){
@@ -48,9 +21,18 @@ require(['jquery','underscore','sudoku','templates'],function($,_,sudoku,templat
         difficulty  = $this.data('difficulty');
     $('#sudokuBoard').sudoku({
       difficulty: difficulty,
-      onComplete: function(board) {
+      onInit: function() {
         $('#difficultyLabel').html(difficulty);
         $('.flippable').addClass('is-flipped');
+      },
+      onPause: function() {
+        console.log('paused');
+      },
+      onRestart: function() {
+        console.log('restarted');
+      },
+      onSolve: function() {
+        console.log('solved');
       }
     });
   })
--- app/scripts/sudoku.js
+++ app/scripts/sudoku.js
@@ -12,20 +12,59 @@ define(['jquery','underscore','utilities','templates'],function($,_,utilities,te
 
     // Options
 		var options = $.extend({
-			onComplete: function(board) { },
-			difficulty: "easy",
-      html: true
+			onInit: function() { },
+			difficulty: "easy"
 		}, options);
 
     var createBoard = function createBoard($elem)
     {
-      var board = generateBoard(DIFFICULTY[options.difficulty]);
-      if (options.html)
-        $elem.html(boardToHtml(board));
-      options.onComplete(board);
+      var sudokuBoard = {};
+      sudokuBoard.solved = shuffleBoard(generateBoard());
+      sudokuBoard.cleared = clearBoard(sudokuBoard.solved, DIFFICULTY[options.difficulty]);
+      sudokuBoard.solve = function() {$elem.html(solveBoard(sudokuBoard.solved));};
+      sudokuBoard.restart = function() {$elem.html(solveBoard(sudokuBoard.cleared));};
+      $elem.html(boardToHtml(sudokuBoard.cleared));
+      $elem.data('sudoku',sudokuBoard);
+      bindSquares($elem);
+      options.onInit();
     };
 
-    var generateBoard = function generateBoard(difficulty)
+    var bindSquares = function bindSquares($elem) {
+      $elem.on('input','.sudoku-square',function(e){
+        var $this = $(this);
+        var $enteredValue = $this.html();
+        if ($enteredValue.length > 1)
+          {
+            //shake box
+          }
+        $this.html($enteredValue.slice(0,1));
+      })
+      .on('keydown','.sudoku-square',function(e){
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+             // Allow: Ctrl+A
+            (e.keyCode == 65 && e.ctrlKey === true) ||
+             // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+                 // let it happen, don't do anything
+
+                 if (e.keyCode === 8)
+                   $(this).html("");
+                 return;
+        }
+        // Ensure that it is a number and stop the keypress (1-9)
+        if ((e.shiftKey || (e.keyCode < 49 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+      });
+    }
+
+    var solveBoard = function(solvedBoard)
+    {
+      return boardToHtml(solvedBoard);
+    }
+
+    var generateBoard = function generateBoard()
     {
       var n = 3;
       var board = new Array(n*n);
@@ -37,8 +76,6 @@ define(['jquery','underscore','utilities','templates'],function($,_,utilities,te
               board[i][j] = Math.floor((i*n + i/n + j) % (n*n) + 1);
             }
         }
-      board = shuffleBoard(board);
-      board = clearBoard(board, difficulty);
       return board;
     };
 
@@ -50,6 +87,7 @@ define(['jquery','underscore','utilities','templates'],function($,_,utilities,te
 
     var shuffleBoard = function shuffleBoard(board)
     {
+      var shuffledBoard = board;
       for (var times = 0; times < 50; times++)
         {
           var n1 = Math.ceil(Math.random() * 9);
@@ -58,32 +96,36 @@ define(['jquery','underscore','utilities','templates'],function($,_,utilities,te
             n2 = Math.ceil(Math.random() * 9);
           }
 
-          for (var i = 0; i < board.length; i++)
+          for (var i = 0; i < shuffledBoard.length; i++)
             {
-              for (var j = 0; j < board[i].length; j++)
+              for (var j = 0; j < shuffledBoard[i].length; j++)
                 {
-                  if (board[i][j] == n1)
-                      board[i][j] = n2;
-                  else if (board[i][j] == n2)
-                      board[i][j] = n1;
+                  if (shuffledBoard[i][j] == n1)
+                      shuffledBoard[i][j] = n2;
+                  else if (shuffledBoard[i][j] == n2)
+                      shuffledBoard[i][j] = n1;
                 }
             }
         }
-        return board;
+        return shuffledBoard;
     };
 
     var clearBoard = function clearBoard(board, difficulty)
     {
+      var clearedBoard = new Array(board.length);
       var difficultyArray = getDifficultyArray(difficulty);
       for (var i = 0; i < 9; i++)
         {
+          clearedBoard[i] = new Array(board[i].length)
           for (var j = 0; j < 9; j++)
             {
-              if (!utilities.contains(difficultyArray,Math.floor((i*9 + i/9 + j)+ 1)))
-                board[i][j] = "";
+              if (utilities.contains(difficultyArray,Math.floor((i*9 + i/9 + j)+ 1)))
+                clearedBoard[i][j] = board[i][j];
+              else
+                clearedBoard[i][j] = "";
             }
         }
-      return board;
+      return clearedBoard;
     };
 
     var getDifficultyArray = function difficultyArray(difficulty)
